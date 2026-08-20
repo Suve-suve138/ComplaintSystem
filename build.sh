598aa7784d82d42dfb7fe5072201cf35b1bf6f73
Suvechha Pradhan <pradhansuvechha502@gmail.com>
2026-08-20 13:12:25 +0530

    Force LF line endings for build.sh and automate superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Create a superuser automatically using environment variables if they are set
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
